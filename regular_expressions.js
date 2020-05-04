d76159417972f515cae5c4e266c71e0ebe3ff4e5
Strigida <strigidacorvidae@gmail.com>
2020-05-04 14:04:25 -0700

    Day 4
    Continued learning of regular expressions at freeCodeCamp.

--- regular_expressions.js
+++ regular_expressions.js
@@ -35,3 +35,51 @@ let resultGTag = twinkleStar.match(starRegex);
 let resultGTagTest = starRegex.test(twinkleStar);
 
 console.log(resultGTagTest + ", " + resultGTag);
+
+//the dot/period character is a wildcard marker, allowing it to match any letter. 
+//for example, "fun" could instead have been "bun", "sun", or any other variation, including misspellings
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let resultWildTest = unRegex.test(exampleStr);
+let resultWildMatch = exampleStr.match(unRegex);
+
+console.log(resultWildTest + ", " + resultWildMatch);
+
+//a character class ([abcd]) looks for all characters within the square brackets
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; //
+let resultCharacter = quoteSample.match(vowelRegex);
+
+console.log(resultCharacter);
+
+//match a range of characters (alphabetical or numeric) inside the square brackets by hyphenating them
+let quoteSample2 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let AlphaResult = quoteSample2.match(alphabetRegex);
+
+console.log(AlphaResult);
+
+//a range can be concatenated to search for multiple ranges
+let quoteSample3 = "Blueberry 3.141592653s are delicious.";
+let conCatRegex = /[h-s2-6]/gi; // Change this line
+let conCatResult = quoteSample3.match(conCatRegex);
+
+console.log(conCatResult);
+
+//negated character sets
+//add a carrot (^) into the square brackets ahead of the characters you DON'T want found
+//this regex excludes all vowels and numbers
+let negString = "001 Why, sometimes I've believed as many as six impossible things before breakfast!";
+let negRegex = /[^aeiou0-9]/gi;
+let negResult = negString.match(negRegex);
+
+console.log("everything that isn't negated is: " + negResult);
+
+//to find characters that occur more than once (in a row) add "+" directly after the character you want checked
+let difficultSpelling = "Mississippi";
+//if there's only one set of multiple characters being tested for, the square brackets aren't needed (as it's not a set)
+let diffRegex = /[s+p+]/gi; // Change this line
+let diffResult = difficultSpelling.match(diffRegex);
+
+console.log(difficultSpelling + ": " + diffResult);
+
