de54ff8119db25d7ee457ba3ccbe55de0491bf3b
Strigida <strigidacorvidae@gmail.com>
2020-05-07 12:45:11 -0700

    Day 7
    Started a game/social networking prototype in HTML and JS with a node interface (in case of testing needs).

---
+++ sittingRoom/virtualSittingRoomProto.js
@@ -0,0 +1,8 @@
+var climate = "temperate";
+var home = "farmHouse";
+var homeStories = 1;
+var rooms = ["Sitting Room", "Kitchen", "Bedroom", "Dining room", "Bathroom", "Library", "Study", "Garage"];
+var furniture = [];
+var furnStyle = "traditional";
+
+function beginDesign()
