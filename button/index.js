ac7ca441ced5a59936a4fe16d73b83fde4b6e5ae
Strigida <strigidacorvidae@gmail.com>
2020-06-19 13:03:17 -0700

    APIs for quote and image functioning.

--- button/index.js
+++ button/index.js
@@ -1,15 +1,15 @@
-//feed randomized advice into web-page based on radio button and click
-//get radio input
-//get button
-function changeImageText() {
-	//document.getElementById("myImage").innerHTML = "New Words Appear!";
-	if(document.getElementById("adviceImage").src == "https://i0.pickpik.com/photos/908/101/183/eye-iris-algae-macro-preview.jpg") {
-		document.getElementById("adviceImage").src = "https://i1.pickpik.com/photos/475/109/77/cd-cover-fantasy-hands-bubble-preview.jpg";
-	} else {
-		document.getElementById("adviceImage").src = "https://i0.pickpik.com/photos/908/101/183/eye-iris-algae-macro-preview.jpg";
-	}
-	console.log("Finished function, yeah!");
+//feed randomized advice into web-page on button click
+ 
+//images from https://picsum.photos
+function randomImage() {
+	document.getElementById("adviceImage").src = "https://picsum.photos/500/350";
+	randomQuote();
 }
-//randomize quotes from advice topic selected
-//output advice to div in web page
-//allow to be done multiple times
+
+//api solution from: https://github.com/lukePeavey/quotable#get-random-quote
+async function randomQuote() {
+  const response = await fetch('https://api.quotable.io/random');
+  const data = await response.json();
+  document.getElementById("customAdvice").innerHTML = (`${data.content}` + "<br /> - " + `${data.author}`);
+}
+
